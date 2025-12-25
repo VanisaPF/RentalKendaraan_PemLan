358887e95b04b88f9b1a59197a77e69f0486c5ef
Vanisa <vanisaputrifransisca@gmail.com>
2025-12-26 06:59:44 +0700

    Add files via upload

---
+++ AppColors.java
@@ -0,0 +1,15 @@
+package com.praktikum.GUI;
+
+import java.awt.*;
+
+class AppColors {
+    static final Color NAVY = new Color(0, 32, 63);
+    static final Color GOLD = new Color(212, 175, 55);
+    static final Color LIGHT_NAVY = new Color(28, 55, 90);
+    static final Color CREAM = new Color(255, 250, 240);
+    static final Color DEEPNAVY = new Color(15, 30, 60);
+    static final Color ROYALGOLD = new Color(184, 134, 11);
+    static final Color NAWA= new Color(158, 197, 210, 242);
+    static final Color WHITE = Color.WHITE;
+    static final Color DAY_SKY_BLUE= new Color(130, 202, 255, 255);
+}
